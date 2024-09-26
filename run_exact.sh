485417722917a6a6473d5e7d84968762cc04e2d0
Ronalito <rscortessa@unal.edu.co>
2024-09-27 00:04:28 +0200

    added

---
+++ run_exact.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+N=$1
+Gamma=$2
+GammaF=$3
+n_sample=$4
+n_run=$5
+n_mean=$6
+each=$7
+NG=$8
+# sbatch -n4
+# sbatch -p regular2 
+source /home/rcortess/anaconda3/etc/profile.d/conda.sh
+conda activate workforce
+
+python3 exact_obv.py $N $Gamma $GammaF $n_sample $n_run $n_mean $each $NG 
+
