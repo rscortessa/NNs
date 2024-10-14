35d20afe3075af2717019c24ebc4cff1ec59aab6
Ronalito <rscortessa@unal.edu.co>
2024-10-15 00:33:38 +0200

    new.pys

---
+++ exact_class.py
@@ -0,0 +1,71 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import class_WF
+import var_nk
+from scipy.sparse.linalg import eigsh    
+import netket as nk
+import sys
+
+## CONSTANTS
+eps=10**(-8)
+dx=0.01
+V=-1.0
+n_method=3
+
+## PARAMETERS
+parameters=sys.argv
+n_par=len(parameters)
+parameters=[int(parameters[x]) for x in range(1,n_par)]
+print(parameters)
+L=parameters[0]
+Gamma=parameters[1]*(-dx)
+GammaF=parameters[2]*(-dx)
+n_samples=parameters[3]
+n_run=parameters[4]
+n_mean=parameters[5]
+NG=parameters[7]
+
+#INSERT PUBLISHER DETAILS AND INITIALIZE IT
+name_var=["M","L","NS","NR","G","GF"]
+var=[n_method,L,n_samples,n_run,parameters[1],parameters[2]]
+variables=["S","E","Id"]
+pub=class_WF.publisher(name_var,var,variables)
+pub.create()
+
+#INITIALIZE OBJECTS
+hi=nk.hilbert.Spin(s=1 / 2,N=L)
+H=class_WF.Ham(Gamma,V,L,hi)
+eig_vecs=class_WF.Diag(H)
+E_WF=class_WF.WF(L,var_nk.EWF(eig_vec=tuple(np.abs(eig_vecs[:,0])),L=L),H,n_samples)
+
+#ITERATION OVER THE GAMMA VALUES:
+
+for gg in range(NG):
+    G=Gamma+(GammaF-Gamma)/NG*gg
+    H=class_WF.Ham(G,-1.0,L,hi)
+    eig_vecs=class_WF.Diag(H)
+    sampler=nk.sampler.MetropolisLocal(hi)
+    aux=np.zeros((n_samples,3))
+    E_WF.change_H(H)
+    for hh in range(n_samples):
+        New_state=nk.vqs.MCState(sampler,var_nk.EWF(eig_vec=tuple(np.abs(eig_vecs[:,0])),L=L),n_samples=n_samples)
+        E_WF.change_state(New_state)
+        A=E_WF.sampling()
+        aux[hh,0]=E_WF.compute_PCA(eps,A=A)
+        aux[hh,1]=E_WF.compute_E()
+        aux[hh,2]=E_WF.compute_ID(eps,A=A)
+    
+    dS=np.std(aux[:,0])/np.sqrt(n_samples)
+    S=np.mean(aux[:,0])
+    dE=np.std(aux[:,1])/np.sqrt(n_samples)
+    E=np.mean(aux[:,1])
+    dID=np.std(aux[:,2])/np.sqrt(n_samples)
+    ID=np.mean(aux[:,2])
+    pub.write([dS,S,dE,E,dID,ID])
+
+pub.close()
+
+
+
+
+
---
+++ var_nk.py
@@ -0,0 +1,54 @@
+import netket as nk
+import jax.numpy as jnp
+import flax
+import jax
+import flax.linen as nn
+import numpy as np
+
+def change_to_int(x,L):
+    Aux=jnp.array([2**(L-1-i) for i in range(L)])
+    Z=jnp.array(jnp.mod(1+x,3)/2,int)
+    return np.sum(Aux*Z,axis=-1)
+
+
+class MF(nn.Module):                                                                                                                   
+    @nn.compact # What is this ?                                                                                                       
+    def __call__(self,x): #x.shape(Nsamples,L)                                                                                         
+        lam= self.param("lambda", nn.initializers.normal(),(1,),float)                                                                 
+        p= nn.log_sigmoid(lam*x) ## How does the initializers work?                                                                    
+        return 0.5*jnp.sum(p,axis=-1)                                                                                                  
+                                                                                                                                       
+class EWF(nn.Module):                                                                                                                  
+    eig_vec:tuple                                                                                                                      
+    L:float                                                                                                                            
+    def setup(self):                                                                                                                   
+        self.aux=jnp.array(self.eig_vec)                                                                                               
+        self.j1=self.param("j1", nn.initializers.normal(),(1,),float)                                                                  
+                                                                                                                                       
+    def __call__(self,x):                                                                                                              
+        indices = change_to_int(x,self.L)                                                                                              
+        A = [self.aux[idx] for idx in indices]                                                                                         
+        return jnp.log(jnp.array(A))                                                                                                   
+                                                                                                                                       
+class JasShort(nn.Module):                                                                                                             
+    @nn.compact                                                                                                                        
+    def __call__(self,x):                                                                                                              
+        j1=self.param("j1", nn.initializers.normal(),(1,),float)                                                                       
+        j2=self.param("j2", nn.initializers.normal(),(1,),float)                                                                       
+        ## Nearest neighbor correlations                                                                                               
+        corr1=x*jnp.roll(x,-1,axis=-1)                                                                                                 
+        corr2=x*jnp.roll(x,-2,axis=-1)                                                                                                 
+        return jnp.sum(j1*corr1+j2*corr2,axis=-1)
+
+class FFN(nn.Module):
+    alpha : int = 1
+    layers : int = 1
+    @nn.compact
+    def __call__(self, x):
+        aux=x
+        for i in range(self.layers):
+            dense = nn.Dense(features=self.alpha * aux.shape[-1])
+            aux_1=dense(aux)
+            aux = nn.relu(aux_1)
+        y=aux
+        return jnp.sum(y, axis=-1)
