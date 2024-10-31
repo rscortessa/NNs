5ecc520309b97eb0bf687dc33f58379287317d53
Ronalito <rscortessa@unal.edu.co>
2024-10-31 15:15:22 +0100

    added

--- variational_code/densitygrid_class.py
+++ variational_code/densitygrid_class.py
@@ -9,22 +9,15 @@ import netket as nk
 import sys
 
 ## CONSTANTS
+
 eps=10**(-8)
 dx=0.01
 V=-1.0
 n_between=200
 
-t1=1
-t2=5
-
-
-
-## ALMOST CONSTANTS
-n_neurons=1
-n_layers=1
-
 
 ## PARAMETERS
+
 parameters=sys.argv
 n_par=len(parameters)
 parameters=[int(parameters[x]) for x in range(1,n_par)]
@@ -52,15 +45,12 @@ model=methods[n_method]
 #INSERT PUBLISHER DETAILS AND INITIALIZE IT
 
 if n_method==2:
-    name_var=["M","L","NS","NR","G","GF","t1","t2","NN","NL"]
-    var=[n_method,L,n_samples,n_run,parameters[1],parameters[2],t1,t2,n_neurons,n_layers]
+    name_var=["M","L","NS","NR","G","tmax","NN","NL"]
+    var=[n_method,L,n_samples,n_run,parameters[1],tmax,n_neurons,n_layers]
 else:
-    name_var=["M","L","NS","NR","G","GF"]
-    var=[n_method,L,n_samples,n_run,parameters[1],parameters[2]]    
+    name_var=["M","L","NS","NR","G","tmax"]
+    var=[n_method,L,n_samples,n_run,parameters[1],tmax]    
     
-variables=["D"]
-pub=class_WF.publisher(name_var,var,variables)
-pub.create()
 
 #INITIALIZE OBJECTS
 hi=nk.hilbert.Spin(s=1 / 2,N=L)
@@ -70,20 +60,26 @@ E_WF=class_WF.WF(L,model,H,n_samples)
 
 #ITERATION OVER THE GAMMA VALUES:
 E_WF.advance(n_run)
-    
+A=E_WF.sampling()
+
+
+if n_method==0 or n_method==2:
+    lenght=len(A)
+    A[:int(lenght/2),:]=(-1)*A[:int(lenght/2),:]
+
+B,W=TId.sets(A)
+C=TId.neighbors(B)
+D=[None for i in range(len(C))]
+
+
+pub=class_WF.publisher(name_var,var,[])
+pub.create()
+
+pub.write([ "S"+str(var_nk.change_to_int(x,L)) for x in B])
 
 for gg in range(tmax):
-    A=E_WF.sampling()
-    if n_method==0 or n_method==2:
-        lenght=len(A)
-        A[:int(lenght/2),:]=(-1)*A[:int(lenght/2),:]
-    B,W=TId.sets(A)
-    C=TId.neighbors(B)
-    D=TId.n_points(C,W,gg)
-
-dD=np.std(aux[:,0])/np.sqrt(n_mean)
-D=np.mean(aux[:,0])
-pub.write([dD,D])
+    D[gg]=TId.n_points(C,W,gg)
+    pub.write(D[gg])
 
 pub.close()
 
