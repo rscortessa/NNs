9b6ee13540b57b9a77ef1ad098fca34e19ad9492
Ronalito <rscortessa@unal.edu.co>
2024-11-11 15:15:00 +0100

    added

--- MPS.sh
+++ MPS.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 # Prompt for L and N_samples
-read -p "Enter the value of L: " L
-read -p "Enter the number of samples (N_samples): " N_samples
+#read -p "Enter the value of L: " L
+#read -p "Enter the number of samples (N_samples): " N_samples
 
 # Prompt for Gamma range and increment
-read -p "Enter the starting value of Gamma: " Gamma_start
-read -p "Enter the ending value of Gamma: " Gamma_end
-read -p "Enter the increment for Gamma: " Gamma_increment
+#read -p "Enter the starting value of Gamma: " Gamma_start
+#read -p "Enter the ending value of Gamma: " Gamma_end
+#read -p "Enter the increment for Gamma: " Gamma_increment
+L=$1
+N_samples=$2
+Gamma_start=$3
+Gamma_end=$4
+Gamma_increment=$5
+
+echo "Value of L=$L" 
+echo "Starting value of Gamma=$Gamma_start"
+echo "Ending value of Gamma=$Gamma_end"
+echo "Increment of Gamma=$Gamma_increment"
 
 # Loop over Gamma values
 Gamma=$Gamma_start
@@ -15,6 +25,6 @@ while (( $(echo "$Gamma <= $Gamma_end" | bc -l) )); do
     echo "Running with Gamma=$Gamma, L=$L, N_samples=$N_samples"
     julia DMRG_QISING.jl "$L" "$Gamma" "$N_samples"
     python run_MPS.py "$L" "$Gamma" "$N_samples"
-    python dataset_run.py "$L" "Gamma" "$N_samples"
+    python dataset_run.py "$L" "$Gamma" "$N_samples"
     Gamma=$(echo "$Gamma + $Gamma_increment" | bc -l)  # Increment Gamma
 done
