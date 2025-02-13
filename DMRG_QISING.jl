ccb7a5b7242d71b694a7f0bca6271cddd66860fe
Ronalito <rscortessa@unal.edu.co>
2025-02-13 19:12:14 +0100

    added

--- DMRG_QISING.jl
+++ DMRG_QISING.jl
@@ -15,15 +15,15 @@ let
   if W>1
    for i = 0:W-2
      for j = 0:N-1
-      os += -4.0,"Sx",j+1+i*N, "Sx",(j+1)%N+1+i*N
-      os += -4.0,"Sx",j+1+i*N, "Sx",j+1+((i+1)%W)*N
-      os += -h*0.002, "Sz", j+1+i*W
+      os += -4.0,"Sz",j+1+i*N, "Sz",(j+1)%N+1+i*N
+      os += -4.0,"Sz",j+1+i*N, "Sz",j+1+((i+1)%W)*N
+      os += -h*0.002, "Sx", j+1+i*W
      end
    end
   else
    for j = 0:N-1
-     os += -4.0,"Sx",j+1, "Sx",(j+1)%N+1
-     os += -h*0.002, "Sz",j+1
+     os += -4.0,"Sz",j+1, "Sz",(j+1)%N+1
+     os += -h*0.002, "Sx",j+1
    end
   end
   
