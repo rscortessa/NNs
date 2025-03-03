b166902dd1cb679d123a51269d07fc91d03bb3d8
Ronalito <rscortessa@unal.edu.co>
2025-03-03 18:04:13 +0100

    added

--- MULT_MODEL.sh
+++ MULT_MODEL.sh
@@ -13,7 +13,7 @@ for jj in $(seq 1 $N_rep)
 do
   bash ONLY_MODEL.sh $L $W $N_samples $Gamma $MODEL $Delete
   mv "M5L${L}W${W}NS${N_samples}NR1000G${Gamma}D.txt" "${dir}/M5L${L}W${W}NS${N_samples}NR1000G${Gamma}D.txt$jj"
-  if [! "$Delete"] ; then
+  if [ !$Delete ] ; then
       mv "DATAM5L${L}W${W}NS${N_samples}NR1000G${Gamma}.txt" "${dir}/DATAM5L${L}W${W}NS${N_samples}NR1000G${Gamma}.txt$jj"
       mv "DATAM5L${L}W${W}NS${N_samples}MPSG${Gamma}.txt" "${dir}/DATAM5L${L}W${W}NS${N_samples}MPSG${Gamma}.txt$jj"
   fi
