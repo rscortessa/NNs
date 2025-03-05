7a67f62741c85fb83f4eeb87d4c4c388405ca06a
Ronalito <rscortessa@unal.edu.co>
2025-03-05 19:46:41 +0100

    added

--- SAVER_MODEL.sh
+++ SAVER_MODEL.sh
@@ -17,18 +17,35 @@ mkdir "${dir}"
 
 echo "Running with Gamma=$Gamma, L=$L, N_samples=$N_samples"
 
-for (( jj=1;jj<=N_REP;jj++))
-do
-    Gamma=$4  
-    while (( $(echo "$Gamma <= $GammaF" | bc -l) )); do
-        python run_MPS.py "$L" "$W" "$Gamma" "$N_samples" "$jj" 
-        python dataset_density_run.py "$L" "$W" "$Gamma" "$N_samples" "1000" "5" "$jj"
-	Gamma=$(echo "$Gamma + $DG" | bc -l)  # Increment Gamma
-    done
-    
+
+M=4  # Number of parallel processes
+chunk_size=$(( (N_REP + M - 1) / M ))  # Ceiling division
+
+
+for j in $(seq 0 $((M-1))); do
+    start=$(( j * chunk_size + 1 ))
+    end=$(( (j + 1) * chunk_size ))
+    if (( end > N_REP )); then
+        end=$N_REP
+    fi
+
+    echo "Process $j working on $start to $end"
+
+    (
+        for (( jj=$start; jj<=$end; jj++ )); do
+            Gamma=$4
+            while (( $(echo "$Gamma <= $GammaF" | bc -l) )); do
+                python run_MPS.py "$L" "$W" "$Gamma" "$N_samples" "$jj"
+                python dataset_density_run.py "$L" "$W" "$Gamma" "$N_samples" "1000" "5" "$jj"
+                Gamma=$(echo "$Gamma + $DG" | bc -l)  # Increment Gamma
+            done
+        done
+    ) &  # Background process
 done
 
-wait
+wait  # Wait for all parallel processes to finish
+echo "All jobs finished!"
+
 if "$Delete" ; then
     rm DATAM5L${L}W${W}NS${N_samples}NR1000G*.txt*
     rm DATAM5L${L}W${W}NS${N_samples}MPSG*.txt*
