78f6ef46db04318958fadf198e56626c3a320d22
Ronalito <rcortess@sissa.it>
2025-03-14 13:16:09 +0100

    added

---
+++ ONLY_PARALLEL_MODEL.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+L=$1
+W=$2
+N_samples=$3
+Gamma=$4
+GammaF=$5
+DG=$6
+N_REP=$7
+MODEL=$8
+Delete=$9
+
+echo "Running with Gamma=$Gamma, L=$L, N_samples=$N_samples"
+
+julia DMRG_MODELS.jl "$L" "$W" "$Gamma" "$GammaF" "$DG" "$N_samples" "$N_REP" "$MODEL"
+
+dir=${MODEL}L${L}W${W}NS${N_samples}GI${Gamma}GF${GammaF}NR${N_REP}
+mkdir "${dir}"
+
+mv *M5L${L}W${W}NS${N_samples}*.txt* "${dir}/"
+
