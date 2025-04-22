ba22a61c366fcf32e15369858e1ad6ba4d024e27
Ronald-pro <rscortessa@unal.edu.co>
2025-04-22 11:59:04 +0200

    added

--- Methods/class_WF.py
+++ Methods/class_WF.py
@@ -57,6 +57,54 @@ def rotated_IsingModel(angle,Gamma,L,hi):
     for i in range(L):
         H -= Gamma * nk.operator.LocalOperator(hi,pseudo_sigma_x,[i])
     return H
+def rotated_BROKEN_Z2IsingModel(angle,Gamma,L,hi):
+     # Initialize Hamiltonian as a LocalOperator
+    pseudo_sigma_x=rotated_sigmax(angle)
+    pseudo_sigma_z=rotated_sigmaz(angle)
+    H = nk.operator.LocalOperator(hi)
+    eps=10**(-4)
+    # Add 2 body- interactions
+    for i in range(L - 1):
+        H -= nk.operator.LocalOperator(hi, np.kron(pseudo_sigma_z,pseudo_sigma_z), [i, i+1])
+    # Add single body term
+    for i in range(L):
+        H -= Gamma * nk.operator.LocalOperator(hi,pseudo_sigma_x,[i])-eps*nk.operator.LocalOperator(hi,pseudo_sigma_z,[i])
+    return H
+
+
+
+def rotated_TWOIsingModel(angle,Gamma,L,hi):
+     # Initialize Hamiltonian as a LocalOperator
+    pseudo_sigma_x=rotated_sigmax(angle)
+    pseudo_sigma_z=rotated_sigmaz(angle)
+    H = nk.operator.LocalOperator(hi)
+
+    # Add 2 body- interactions
+    for i in range(L - 1):
+        H -= nk.operator.LocalOperator(hi, np.kron(pseudo_sigma_z,pseudo_sigma_z), [i, i+1])
+        H -= nk.operator.LocalOperator(hi, np.kron(pseudo_sigma_z,pseudo_sigma_z), [i, i+1+L])
+    # Add single body term
+    for i in range(2*L):
+        H -= Gamma * nk.operator.LocalOperator(hi,pseudo_sigma_x,[i])
+    return H
+
+def rotated_LONGIsingModel(angle,Gamma,L,hi):
+     # Initialize Hamiltonian as a LocalOperator
+    pseudo_sigma_x=rotated_sigmax(angle)
+    pseudo_sigma_z=rotated_sigmaz(angle)
+    H = nk.operator.LocalOperator(hi)
+
+    # Add 2 body- interactions
+    for i in range(L - 2):
+        H -= nk.operator.LocalOperator(hi, np.kron(pseudo_sigma_z,pseudo_sigma_z), [i, i+1])
+        H -= nk.operator.LocalOperator(hi, np.kron(pseudo_sigma_z,pseudo_sigma_z), [i, i+2])
+        
+    H -= nk.operator.LocalOperator(hi, np.kron(pseudo_sigma_z,pseudo_sigma_z), [L-2,L-1])
+    # Add single body term
+    for i in range(L):
+        H -= Gamma * nk.operator.LocalOperator(hi,pseudo_sigma_x,[i])
+    return H
+
 
 def rotated_XYZModel(angle,Gamma,L,hi):
     
