8fda548492f3c3b3ab3685ee38837870855e4cfb
Ronald-pro <rscortessa@unal.edu.co>
2025-09-09 18:33:09 +0200

    added

--- Methods/FULL_STATE_OP.py
+++ Methods/FULL_STATE_OP.py
@@ -34,3 +34,28 @@ def objective(trial,model,L,hi,H,n_iter,holomorphic):
     score=log.data["Energy"]["Mean"][-1]
     return score
 
+def objective_I(trial,model,psi,phi,L,hi,H,n_iter,holomorphic):    
+    log = nk.logging.RuntimeLog()   
+    learning_rate = trial.suggest_float("learning_rate",10**(-5),10**(-1),log=True)
+    diag_shift = trial.suggest_float("diag_shift",10**(-5),10**(-1),log=True)
+    cv = trial.suggest_float("cv",10**(-5),10**(-1),log=True)
+
+    optimizer = nk.optimizer.Sgd(learning_rate=learning_rate)
+    preconditioner = nk.optimizer.SR(diag_shift=diag_shift, holomorphic=holomorphic)
+
+    te = nkf.driver.InfidelityOptimizer(psi, optimizer, variational_state=phi, preconditioner=preconditioner, cv_coeff=cv)
+
+    log = nk.logging.RuntimeLog()
+    te.run(n_iter=50,out=log,show_progress=False)
+
+    for i in range(60,n_iter,10):
+        te.run(n_iter=10,out=log,show_progress=False)
+        intermediate_score=log.data["Infidelity"]["Mean"][-1]
+        trial.report(intermediate_score,i)
+        # Handle pruning based on the intermediate value.
+        if trial.should_prune():
+            raise optuna.TrialPruned()
+    score=log.data["Infidelity"]["Mean"][-1]
+    
+    return score
+
