203906ffec14f53c0f7f76cb040dd88d65a6d5e6
Ronald-pro <rscortessa@unal.edu.co>
2025-11-02 02:33:22 +0100

    added

--- OPTIMIZED_JASTROW_INFIDELITY_QIM_RBM.py
+++ OPTIMIZED_JASTROW_INFIDELITY_QIM_RBM.py
@@ -70,7 +70,7 @@ NL=1
 NR=parameters[2]
 NSPCA=parameters[3]
 NMEAN=parameters[4]
-NSAMPLES=parameters[5]
+g=parameters[5]
 seed=parameters[6]
 n_iter=200
 #Model Details
@@ -102,129 +102,126 @@ if not os.path.isdir(MASTER_DIR):
     os.mkdir(MASTER_DIR)
     print(f"Directory '{MASTER_DIR}' not found previously but created successfully.")
     
-for g in range(NSAMPLES):
-    SLAVE_DIR="FULL_STATE_RUN_"+basis+"_"+architecture+"NN"+str(NN)+"L"+str(L)+"G"+str(g)+"NSPCA"+str(NSPCA)+add
-    try:
-        os.mkdir(MASTER_DIR+"/"+SLAVE_DIR)
-    except FileExistsError:
-        print(f"Directory "+MASTER_DIR+"/"+SLAVE_DIR+" already exists.")
+SLAVE_DIR="FULL_STATE_RUN_"+basis+"_"+architecture+"NN"+str(NN)+"L"+str(L)+"G"+str(g)+"NSPCA"+str(NSPCA)+add
+try:
+    os.mkdir(MASTER_DIR+"/"+SLAVE_DIR)
+except FileExistsError:
+    print(f"Directory "+MASTER_DIR+"/"+SLAVE_DIR+" already exists.")
 
 #Constants initialization
 
 # Hilbert space generation in Netket
 
+#DEFINE DIRECTORIES
+    
+SLAVE_DIR="FULL_STATE_RUN_"+basis+"_"+architecture+"NN"+str(NN)+"L"+str(L)+"G"+str(g)+"NSPCA"+str(NSPCA)+add
+FILENAME=basis+"M3L"+str(L)+"W1"+"G"+str(g)+"NN"+str(NN)+"NL"+str(NL)+"NR"+str(NR)
 hyper_parameters_ready=False
+try:
 
-for g in range(NSAMPLES):
+    #CHECK IF THE HYPER-PARAMETERS ARE ALREADY THERE
+
+    print("CHECKING PARAMETERS............")
+    with open(MASTER_DIR+"/"+SLAVE_DIR+"/"+FILENAME+"HYP"+".json", "r") as f:
+        data_best = json.load(f)
     
-    #DEFINE DIRECTORIES
-    
-    SLAVE_DIR="FULL_STATE_RUN_"+basis+"_"+architecture+"NN"+str(NN)+"L"+str(L)+"G"+str(g)+"NSPCA"+str(NSPCA)+add
-    FILENAME=basis+"M3L"+str(L)+"W1"+"G"+str(g)+"NN"+str(NN)+"NL"+str(NL)+"NR"+str(NR)
-    hyper_parameters_ready=False
-    try:
+    hyper_parameters_ready=True
 
-        #CHECK IF THE HYPER-PARAMETERS ARE ALREADY THERE
-
-        print("CHECKING PARAMETERS............")
-        with open(MASTER_DIR+"/"+SLAVE_DIR+"/"+FILENAME+"HYP"+".json", "r") as f:
-            data_best = json.load(f)
-    
-        hyper_parameters_ready=True
-
-        print("HYPER-PARAMETERS ALREADY FOUND")
+    print("HYPER-PARAMETERS ALREADY FOUND")
         
-    except:
-        log_best_params=nk.logging.RuntimeLog()
+except:
+    log_best_params=nk.logging.RuntimeLog()
 
 
         #CREATE A LOG TO SAVE THE HYPERPARAMETERS
-    print("Hyper_parameters exist?",hyper_parameters_ready)
+print("Hyper_parameters exist?",hyper_parameters_ready)
 
+for step in range(g+1):
     J_couplings=list([random.random() for j in range(int(L*(L+1)/2))])
     J= np.array(random.sample(J_couplings, k=len(J_couplings)))
-    rows, cols = np.triu_indices(L)
-    idxs=list(zip(rows, cols))
-    J_coeff=np.zeros((L,L))
-
-    for idx in range(len(idxs)):
-        J_coeff[idxs[idx][1],idxs[idx][0]]=J[idx]
-
-    GS=build_jastrow_wf(L,J_coeff,hi)
-
-    if architecture=="RBM_COMPLEX" or architecture == "WSIGNS_RBM_COMPLEX":
-            
-        model=nk.models.RBM(alpha=NN,param_dtype=complex)
-        holomorphic=True
-            
-    elif architecture=="RBM_REAL" or architecture == "WSIGNS_RBM_REAL":
-        model=nk.models.RBM(alpha=NN)
-        holomorphic=False
-    if architecture == "WSIGNS_RBM_COMPLEX" or architecture == "WSIGNS_RBM_REAL":
-        GS=np.abs(GS)
-            
-    # EXACT G.S
-
-    GS[np.abs(GS)<10**(-10)]=0.0
-    GS=np.log(GS)
-    GS[GS!=GS]=-np.inf
-    Exact_GS=EWF(L=L*W,eig_vec=tuple(GS))
-
-
-    phi = nk.vqs.FullSumState(hi, model=model)
-    psi = nk.vqs.FullSumState(hi, model=Exact_GS)
-
-    if hyper_parameters_ready:
-
-        best_params={}
-        best_params["learning_rate"]=data_best["learning_rate"]["value"][g]
-        best_params["diag_shift"]=data_best["diag_shift"]["value"][g]
-        best_params["cv"]=data_best["cv"]["value"][g]
-
-
-    else:
-            
-        optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
-        study = optuna.create_study(direction="minimize",sampler=optuna.samplers.RandomSampler(),pruner=optuna.pruners.MedianPruner())
-        objective_final=partial(objective_I,model=model,psi=psi,phi=phi,L=L*W,hi=hi,n_iter=n_iter,holomorphic=holomorphic)
-        study.optimize(objective_final,n_trials=50)    
-        best_params=study.best_params
-      
-    #OPTIMIZER AND PRECONDITIONER INITIALIZATION
-        
-    optimizer = optimizer=nk.optimizer.Sgd(learning_rate=best_params["learning_rate"])
-    preconditioner = nk.optimizer.SR(diag_shift=best_params["diag_shift"], holomorphic=holomorphic)
-        
-        
-
-    #OBSERVABLES INIT.        
-
-    obs = {}
-
-    #Number of effective steps
-    NR_eff=int(NR/NSPCA)
-
-    for tt in range(NMEAN):
-            
-        #RESTART THE NETWORK
-        phi.init_parameters()
-        te = nkf.driver.InfidelityOptimizer(psi, optimizer, variational_state=phi, preconditioner=preconditioner, cv_coeff=best_params["cv"])
-            
-        #THE OUT LOGS ARE CREATED
-        log = nk.logging.RuntimeLog()
-        log_var = nk.logging.RuntimeLog()
-            
-        for kk in range(NSPCA):
     
-            #FIRST WE COMPUTE THE PARAMETERS
-            save_params(te,kk,log_var)
-            #RUNNING FOR NR_EFF ITERATIONS
-            te.run(obs=obs,n_iter=NR_eff,out=log,show_progress=False)
+rows, cols = np.triu_indices(L)
+idxs=list(zip(rows, cols))
+J_coeff=np.zeros((L,L))
+
+for idx in range(len(idxs)):
+    J_coeff[idxs[idx][1],idxs[idx][0]]=J[idx]
+
+GS=build_jastrow_wf(L,J_coeff,hi)
+
+if architecture=="RBM_COMPLEX" or architecture == "WSIGNS_RBM_COMPLEX":
+            
+    model=nk.models.RBM(alpha=NN,param_dtype=complex)
+    holomorphic=True
+            
+elif architecture=="RBM_REAL" or architecture == "WSIGNS_RBM_REAL":
+    model=nk.models.RBM(alpha=NN)
+    holomorphic=False
+if architecture == "WSIGNS_RBM_COMPLEX" or architecture == "WSIGNS_RBM_REAL":
+    GS=np.abs(GS)
+            
+# EXACT G.S
+
+GS[np.abs(GS)<10**(-10)]=0.0
+GS=np.log(GS)
+GS[GS!=GS]=-np.inf
+Exact_GS=EWF(L=L*W,eig_vec=tuple(GS))
+
+
+phi = nk.vqs.FullSumState(hi, model=model)
+psi = nk.vqs.FullSumState(hi, model=Exact_GS)
+
+if hyper_parameters_ready:
+
+    best_params={}
+    best_params["learning_rate"]=data_best["learning_rate"]["value"][0]
+    best_params["diag_shift"]=data_best["diag_shift"]["value"][0]
+    best_params["cv"]=data_best["cv"]["value"][0]
+
+
+else:
+            
+    optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
+    study = optuna.create_study(direction="minimize",sampler=optuna.samplers.RandomSampler(),pruner=optuna.pruners.MedianPruner())
+    objective_final=partial(objective_I,model=model,psi=psi,phi=phi,L=L*W,hi=hi,n_iter=n_iter,holomorphic=holomorphic)
+    study.optimize(objective_final,n_trials=50)    
+    best_params=study.best_params
+      
+#OPTIMIZER AND PRECONDITIONER INITIALIZATION
+        
+optimizer = optimizer=nk.optimizer.Sgd(learning_rate=best_params["learning_rate"])
+preconditioner = nk.optimizer.SR(diag_shift=best_params["diag_shift"], holomorphic=holomorphic)
+        
+        
+
+#OBSERVABLES INIT.        
+
+obs = {}
+
+#Number of effective steps
+NR_eff=int(NR/NSPCA)
+
+for tt in range(NMEAN):
+            
+    #RESTART THE NETWORK
+    phi.init_parameters()
+    te = nkf.driver.InfidelityOptimizer(psi, optimizer, variational_state=phi, preconditioner=preconditioner, cv_coeff=best_params["cv"])
+            
+    #THE OUT LOGS ARE CREATED
+    log = nk.logging.RuntimeLog()
+    log_var = nk.logging.RuntimeLog()
+            
+    for kk in range(NSPCA):
+    
+        #FIRST WE COMPUTE THE PARAMETERS
+        save_params(te,kk,log_var)
+        #RUNNING FOR NR_EFF ITERATIONS
+        te.run(obs=obs,n_iter=NR_eff,out=log,show_progress=False)
                 
-        #LAST CALCULATION
-        save_params(te,NSPCA,log_var)
-        log_var.serialize(MASTER_DIR+"/"+SLAVE_DIR+"/"+str(tt)+"NM"+str(g)+FILENAME+"VAR")
-        log.serialize(MASTER_DIR+"/"+SLAVE_DIR+"/"+str(tt)+"NM"+str(g)+FILENAME+"OBS")
+    #LAST CALCULATION
+    save_params(te,NSPCA,log_var)
+    log_var.serialize(MASTER_DIR+"/"+SLAVE_DIR+"/"+str(tt)+"NM"+str(g)+FILENAME+"VAR")
+    log.serialize(MASTER_DIR+"/"+SLAVE_DIR+"/"+str(tt)+"NM"+str(g)+FILENAME+"OBS")
 
 
 
