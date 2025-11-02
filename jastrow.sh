52b04d6823c980276ccff580113590f70b3ae1ef
Ronald-pro <rscortessa@unal.edu.co>
2025-11-02 02:51:56 +0100

    added

---
+++ jastrow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+start=0
+L=$1
+NN=$2
+NR=$3
+NSPCA=$4
+NMEAN=$5
+end=$6
+seed=$7
+
+
+python OPTIMIZATION_SEARCH_JASTROW.py $L $NN $NR $NSPCA $NMEAN $end $seed
+
+for ((i=start; i<end; i++))
+do
+  python OPTIMIZED_JASTROW_INFIDELITY_QIM_RBM.py $L $NN $NR $NSPCA $NMEAN $i $seed
+done
